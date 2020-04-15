27e3fd1c060281cc492aa4b9245823e79ab32495
pryvedennyi <pryvedennyi@gmail.com>
2020-04-15 18:02:09 -0400

    second commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Bojour");
+
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
